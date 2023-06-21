84b114d7ad2a828363abe3ed89dd1ca26b326554
Francesco Daghero <francesco.daghero@polito.it>
2023-06-21 15:15:03 +0200

    refactor quantization (wip)

--- src/eden/optimization/quantization.py
+++ src/eden/optimization/quantization.py
@@ -17,157 +17,212 @@
 # * Author: Francesco Daghero francesco.daghero@polito.it                    *
 # *--------------------------------------------------------------------------*
 
-from typing import Mapping, Optional, Tuple, Union, List
-from copy import deepcopy
+from sklearn.base import BaseEstimator
+from typing import Literal, Tuple
 import numpy as np
-from . import _DEPLOYMENT_KEY
+from copy import deepcopy
+from sklearn.tree import _tree, DecisionTreeClassifier, BaseDecisionTree
 
 
-def infer_dtype(*, quant_range: Tuple[int, int]) -> np.dtype:
-    supported_dtype = [np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32]
-    signed = quant_range[0] < 0
-    for dt in supported_dtype:
-        if np.iinfo(dt).min <= quant_range[0] and np.iinfo(dt).max >= quant_range[1]:
-            return dt
-    raise NotImplementedError(f"quant_range={quant_range} is not supported")
+def get_qparams(*, min_val: float, max_val: float, bits: Literal[8, 16, 32]):
+    """
+    Get a dictionary with scale and zero point selected
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Data to be quantized
+    min_val , max_val : float
+        Range of the data to be quantized
+    bits : Literal[8,16,32]
+        QData bit width.
+    method : str
+        Function to apply after scale and zero point, default is clip
+
+    Returns
+    -------
+    Dict
+        Qparams of the quantization
+    """
+    qmin = 0
+    qmax = 2 ** (bits) - 1
+    S = (max_val - min_val) / (2**bits - 1)
+    Z = -(round(min_val / S) - qmin)
+    return {"scale": S, "zero_point": Z}
 
 
-def _get_bits_boundaries(*, signed: bool, bits: int) -> Tuple[int, int]:
-    if signed:
-        qmin = -(2 ** (bits - 1))
-        qmax = 2 ** (bits - 1) - 1
-    else:
-        qmin = 0
-        qmax = 2 ** (bits) - 1
-    return qmin, qmax
-
-
-def get_quantization_parameters(
+def quantize(
     *,
-    data_range: Tuple[float, float],
-    bits: int,
-    quantization_mode: str = "",
-    signed: bool = False,
-) -> Mapping[str, float]:
-    qmin, qmax = _get_bits_boundaries(signed=signed, bits=bits)
-    A, B = data_range
-    S = (B - A) / (2**bits - 1)
-    Z = -(round(A / S) - qmin)
-    return {"S": S, "Z": Z}
+    data: np.ndarray,
+    min_val: float,
+    max_val: float,
+    bits: Literal[8, 16, 32] = 32,
+    method: str = "clip",
+) -> np.ndarray:
+    """
+    Base quantization function for thresholds and inputs.
 
+    Parameters
+    ----------
+    data : np.ndarray
+        Data to be quantized
+    min_val , max_val : float
+        Range of the data to be quantized
+    bits : Literal[8,16,32]
+        QData bit width.
+    method : str
+        Function to apply after scale and zero point, default is clip
 
-def quantizer(
-    *,
-    data,
-    output_bits: int,
-    data_range: Optional[Tuple[float, float]] = None,
-    quantization_mode: str = "uniform",
-    signed: bool = False,
-) -> Tuple[np.ndarray, Mapping[str, float]]:
-    if quantization_mode != "uniform":
-        raise NotImplementedError(
-            f"quantization_mode = {quantization_mode} not supported"
-        )
-    if data_range is None:
-        data_range = (data.min(), data.max())
-
-    qmin, qmax = _get_bits_boundaries(signed=signed, bits=output_bits)
-
-    quant_params: Mapping[str, float] = get_quantization_parameters(
-        data_range=data_range,
-        bits=output_bits,
-        quantization_mode=quantization_mode,
-        signed=signed,
-    )
+    Returns
+    -------
+    np.ndarray
+        A quantized copy of data
+    """
     data = np.copy(data)
-    # TODO: Avoid truncating, but round instead. The sum of multiple tree logits leads
-    # to overflow in C, truncating currently avoids it.
-    data = np.trunc(data / quant_params["S"] + quant_params["Z"]).astype(int)
+    qmin = 0
+    qmax = 2 ** (bits) - 1
+    S = (max_val - min_val) / (2**bits - 1)
+    Z = -(round(min_val / S) - qmin)
+    if method == "clip":
+        data = np.round(data / S + Z).astype(int)
+    else:
+        data = np.trunc(data / S + Z).astype(int)
     data = np.clip(a=data, a_min=qmin, a_max=qmax)
-    return data, quant_params
+    return data
 
 
-def quantize_estimator(
+def quantize_alphas(
     *,
-    quantization_aware_training: bool,
-    estimator_dict: Mapping,
-    input_range: Tuple[float, float],
-    bits_input: Optional[int],
-    bits_output: Optional[int],
-) -> Union[Optional[Mapping], Optional[Mapping], Tuple[Mapping, Mapping]]:
-    # No quantization specified
-    if bits_input is None and bits_output is None:
-        return estimator_dict
-    quantized_ensemble = deepcopy(estimator_dict)
-    quantized_trees = quantized_ensemble["trees"]
+    estimator: BaseEstimator,
+    input_min_val: float,
+    input_max_val: float,
+    method: Literal["post-fit", "qat"] = "post-fit",
+    bits: Literal[8, 16, 32] = 8,
+) -> BaseEstimator:
+    """
+    Quantize the thresholds (alphas) in the input estimator.
 
-    input_qparams, output_qparams = None, None
-    if bits_input is not None:
-        # Extract quantization range
-        quantized_trees, input_qparams = _quantize_ensemble_thresholds(
-            quantization_aware_training=quantization_aware_training,
-            trees_list=quantized_trees,
-            bits=bits_input,
-            input_range=input_range,
+    Parameters
+    ----------
+    estimator : BaseEstimator
+        The estimator to be modified
+    input_min_val, input_max_val : float
+        Range of the input data.
+    method : str, optional
+        Quantization method, "post-fit" for post-training, "qat" for quant-aware
+          training, by default "post-fit"
+    bits : Literal[8, 16, 32], optional
+        Quantization bits of the thresholds, by default 8
+
+    Returns
+    -------
+    BaseEstimator
+        A copy of estimator with quantized alphas.
+    """
+    assert hasattr(estimator, "estimators_") or hasattr(
+        "tree_"
+    ), "Invalid or not trained model, call .fit() first"
+    qestimator = deepcopy(estimator)
+    # Ensemble - GBT
+    if hasattr(estimator, "tree_") and not hasattr(estimator, "estimators_"):
+        __quantize_tree_alphas(
+            qestimator,
+            min_val=input_min_val,
+            max_val=input_max_val,
+            bits=bits,
+            method=method,
         )
-        quantized_ensemble["input_qparams"] = input_qparams
-    if bits_output is not None:
-        quantized_trees, output_qparams = _quantize_ensemble_output(
-            trees_list=quantized_trees, bits=bits_output
-        )
-        quantized_ensemble["output_qparams"] = output_qparams
-    quantized_ensemble["trees"] = quantized_trees
-    return quantized_ensemble
-
-
-def _quantize_ensemble_thresholds(
-    *,
-    quantization_aware_training: bool,
-    trees_list: List[Mapping],
-    bits: int,
-    input_range: Tuple[float, float],
-):
-    trees_list = deepcopy(trees_list)
-    for idx, tree in enumerate(trees_list):
-        if quantization_aware_training:
-            qparams = None
-            qdata = np.ceil(np.asarray(tree["threshold"])).astype(int)
-        else:
-            qdata, qparams = quantizer(
-                data=np.asarray(tree["threshold"]),
-                output_bits=bits,
-                data_range=input_range,
+    else:
+        for tree in np.asarray(estimator.estimators_).reshape(-1):
+            __quantize_tree_alphas(
+                tree,
+                min_val=input_min_val,
+                max_val=input_max_val,
+                bits=bits,
+                method=method,
             )
-        tree["threshold"] = qdata.astype(int).tolist()
-    return trees_list, qparams
+    return qestimator
 
 
-def _quantize_ensemble_output(*, trees_list: List[Mapping], bits: int) -> List[Mapping]:
-    # Get the ranges
-    trees_list = deepcopy(trees_list)
-    extrema = np.zeros((len(trees_list), trees_list[0]["leaf_len"], 2))
-    for idx, tree in enumerate(trees_list):
-        leaf_feature_value = tree["eden_leaf_indicator"]
-        leaf_idx = np.asarray(tree["feature"]) == leaf_feature_value
-        assert leaf_idx.sum() == tree["n_leaves"], "Invalid number of leaves"
-        values = np.asarray(tree["value"])[leaf_idx]
-        # Case 1: Classification OVA, Regression, Binary classification
-        extrema[idx, :, 0] = np.min(values, axis=0)
-        extrema[idx, :, 1] = np.max(values, axis=0)
-    extrema = extrema.sum(axis=0)  # Simulate accumulation
-    extrema = (np.min(extrema), np.max(extrema))
-    print("Detected extrama", extrema)
-
-    qparams = get_quantization_parameters(
-        data_range=extrema, bits=bits, quantization_mode=""
-    )
-    # Actual quantization of the trees and reserialization
-    for idx, tree in enumerate(trees_list):
-        # TODO: Find a better way to implement this. Maybe set them to -1?
-        # For now values != leaf are ignored, so they MUST be ignored in the
-        # quantized dictionary
-        qdata, _ = quantizer(
-            data=np.asarray(tree["value"]), data_range=extrema, output_bits=bits
+def __quantize_tree_alphas(
+    *, tree: BaseDecisionTree, min_val: float, max_val: float, bits: int, method: str
+):
+    basetree = tree.tree_
+    leaves_idx = basetree.children_left != _tree.TREE_LEAF
+    if method == "qat":
+        basetree.threshold[leaves_idx] = np.ceil(basetree.threshold[leaves_idx])
+    else:
+        basetree.threshold[leaves_idx] = quantize(
+            basetree.threshold[leaves_idx], min_val=min_val, max_val=max_val, bits=bits
         )
-        tree["value"] = qdata.astype(int).tolist()
-    return trees_list, qparams
+
+
+#######################################################################################
+## Output quantization
+
+
+def __extract_tree_leaves(estimator):
+    base_tree = deepcopy(estimator.tree_)
+
+    # Remove the unused middle dimension
+    leaf_value = np.squeeze(base_tree.value).reshape(
+        len(base_tree.threshold), -1
+    )  # [N_NODES, N_CLASSES]
+    leaf_value = leaf_value[
+        base_tree.children_left == _tree.TREE_LEAF
+    ]  # [N_LEAVES, 1, N_CLASSES]
+
+    # Classification tree only (excludes GBT for classification)
+    if isinstance(estimator, DecisionTreeClassifier):
+        # Save only the probabilities, not the sample counts
+        leaf_value = leaf_value / leaf_value.sum(axis=1)[:, None]
+        if estimator.n_classes_ == 2:
+            # Reshape is needed to ensure [N_NODES, N_CLASSES]
+            leaf_value = leaf_value[:, 1].reshape(leaf_value.shape[0], 1)
+    return leaf_value
+
+
+def __get_leaf_extrema(estimator) -> Tuple[float, float]:
+    if hasattr(estimator, "tree_") and not hasattr(estimator, "estimators_"):
+        leaf_shape = (
+            estimator.tree_.value.shape[-1]
+            if not hasattr(estimator.tree_, "n_classes_")
+            else 1
+        )
+        extrema = np.zeros((1, leaf_shape, 2))
+        leaf_value = __extract_tree_leaves(estimator=estimator)
+
+        extrema[0, :, 0] = np.min(leaf_value, axis=0)
+        extrema[0, :, 1] = np.max(leaf_value, axis=0)
+    else:
+        leaf_shape = estimator.estimators_[0].tree_.value.shape[-1]
+        extrema = np.zeros(
+            (len(np.asarray(estimator.estimators_).reshape(-1)), leaf_shape, 2)
+        )
+        for idx, tree in enumerate(np.asarray(estimator.estimators_).reshape(-1)):
+            leaf_value = __extract_tree_leaves(estimator=tree)
+
+            extrema[idx, :, 0] = np.min(leaf_value, axis=0)
+            extrema[idx, :, 1] = np.max(leaf_value, axis=0)
+
+        if hasattr(estimator, "classes_") and leaf_shape != estimator.n_classes_:
+            extrema = extrema.reshape(
+                len(estimator.estimators_), estimator.n_classes_, 2
+            )
+    if extrema.shape[1] == 2:
+        extrema = extrema[:, 1, :]
+    if hasattr(estimator, "init") and estimator.init != "zero":
+        extrema += estimator.init_[None, :, None]
+
+    extrema = extrema.sum(axis=0)
+    output_min, output_max = extrema.min(), extrema.max()
+    return output_min, output_max
+
+
+def get_output_qparams(estimator):
+    assert hasattr(estimator, "estimators_") or hasattr(
+        "tree_"
+    ), "Invalid or not trained model, call .fit() first"
+    qestimator = deepcopy(estimator)
+    output_min, output_max = __get_leaf_extrema(qestimator)
+    return output_min, output_max
